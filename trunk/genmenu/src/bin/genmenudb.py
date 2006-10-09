b5e630471e8f032298218e99dd25589e8aa271d0
grimmlin <grimmlin@users.noreply.github.com>
2006-10-09 14:41:11 +0000

    Updated db

--- trunk/genmenu/src/bin/genmenudb.py
+++ trunk/genmenu/src/bin/genmenudb.py
@@ -1,114 +1,114 @@
 #!/bin/python
 
-["net-analyzer/absinthe", "absinthe.eap", "Database"]
-["net-analyzer/aimsniff", "aimsniff.eap", "Analyzer"]
-["net-analyzer/amap", "amap.eap", "Fingerprint"]
-["net-analyzer/arpwatch", "arpwatch.eap", "Analyzer"]
-["net-analyzer/authforce", "authforce.eap", "Cracker-Bruter"]
-["net-analyzer/autoscan", "autoscan.eap", "Scanners"]
-["net-analyzer/bmon", "bmon.eap", "Analyzer"]
-["net-analyzer/cheops-ng", "cheops-ng.eap" "Scanners"]
-["net-analyzer/dnsa", "dnsa.eap", "Analyzer"]
-["net-analyzer/dnsenum", "dnsenum.eap", "Analyzer"]
-["net-analyzer/driftnet", "driftnet.eap", "Analyzer"]
-["net-analyzer/dsniff", "dsniff.eap", "MITM"]
-["net-analyzer/ethereal", "ethereal.eap", "Analyzer"]
-["net-analyzer/ettercap", "ettercap.eap", "MITM"]
-["net-analyzer/firewalk", "firewalk.eap", "Scanners"]
-["net-analyzer/fragroute", "fragroute.eap", "Forging-Spoofing"]
-["net-analyzer/ftester", "ftester.eap", "Stress"]
-["net-analyzer/gnome-nettool", "gnome-nettool.eap", "Analyzer"]
-["net-analyzer/gspoof", "gspoof.eap", "Forging-Spoofing"]
-["net-analyzer/honeyd", "honeyd.eap", "Misc"]
-["net-analyzer/hping", "hping.eap", "Forging-Spoofing"]
-["net-analyzer/hunt", "hunt.eap", "Forging-Spoofing"]
-["net-analyzer/hydra", "hydra.eap", "Cracker-Bruter"]
-["net-analyzer/ike-scan", "ike-scan.eap", "Scanners"]
-["net-analyzer/isic", "isic.eap", "Forging-Spoofing"]
-["net-analyzer/macchanger", "macchanger.eap", "Misc"]
-["net-analyzer/mbrowse", "mbrowse.eap", "Analyzer"]
-["net-analyzer/metacoretex-ng", "metacoretex-ng.eap", "Database"]
-["net-analyzer/metasploit", "metasploit.eap", "Exploits"]
-["net-analyzer/nbtscan", "nbtscan.eap", "Scanners"]
-["net-analyzer/nessus", "nessus.eap", "Scanners"]
-["net-analyzer/net-snmp", "net-snmp.eap", "Analyzer"]
-["net-analyzer/netdude", "netdude.eap", "Analyzer"]
-["net-analyzer/netwag", "netwag.eap", "Forging-Spoofing"]
-["net-analyzer/nikto", "nikto.eap", "Scanners"]
-["net-analyzer/nmap", "nmap.eap", "Scanners"]
-["net-analyzer/nmbscan", "nmbscan.eap", "Scanners"]
-["net-analyzer/ntop", "ntop.eap", "Analyzer"]
-["net-analyzer/ntp-fingerprint", "ntp-fingerprint.eap", "Fingerprint"]
-["net-analyzer/p0f", "p0f.eap", "Fingerprint"
-["net-analyzer/packit", "packit.eap", "Forging-Spoofing"]
-["net-analyzer/paketto", "paketto-linkcat.eap paketto-minewt.eap paketto-paratrace.eap paketto-scanrand.eap", "Forging-Spoofing"]
-["net-analyzer/rain", "rain.eap", "Forging-Spoofing"]
-["net-analyzer/sara", "sara.eap", "Scanners"]
-["net-analyzer/scanssh", "scanssh.eap", "Fingerprint"]
-["net-analyzer/siphon", "siphon.eap", "Fingerprint"]
-["net-analyzer/smtpmap", "smtpmap.eap", "Fingerprint"]
-["net-analyzer/snmpenum", "snmpenum.eap", "Analyzer"]
-["net-analyzer/snort", "snort.eap", "Misc"]
-["net-analyzer/sqlat", "sqlat.eap", "Database"]
-["net-analyzer/sqlbf", "sqlbf.eap", "Database"]
-["net-analyzer/sqlinject", "sqlinject.eap", "Database"]
-["net-analyzer/tcptraceroute", "tcptraceroute.eap", "Analyzer"]
-["net-analyzer/thcrut", "thcrut.eap", "Analyzer"]
-["net-analyzer/traceroute", "traceroute.eap", "Analyzer"]
-["net-analyzer/upnpscan", "upnpscan.eap", "Scanners"]
-["net-analyzer/xprobe", "xprobe.eap", "Fingerprint"]
-["net-analyzer/yersinia", "yersinia.eap", "Exploit"]
-["net-analyzer/zodiac", "zodiac.eap", "Analyzer"]
-["net-firewall/firehol", "firehol.eap", "Misc"]
-["net-firewall/fwbuilder", "fwbuilder.eap", "Misc"]
-["net-ftp/ftpd", "ftpd.eap", "Services"]
-["net-ftp/oftpd", "oftpd.eap", "Services"]
-["net-im/gaim", "gaim.eap", "Misc"]
-["net-im/skype", "skype.eap", "Internet"]
-["net-irc/irssi", "irssi.eap", "Internet"]
-["net-irc/xchat", "xchat.eap", "Internet"]
-["net-misc/bridge-utils", "bridge-utils.eap", "MITM"]
-["net-misc/curl", "curl.eap", "Analyzer"]
-["net-misc/dhcpcd", "dhcpcd.eap", "Misc"]
-["net-misc/etherwake", "etherwake.eap", "Misc"]
-["net-misc/ipsorcery", "ipsorcery.eap", "Forging-Spoofing"]
-["net-misc/minisip", "minisip.eap", "SIP-VoIP"]
-["net-misc/nemesis", "nemesis.eap", "Forging-Spoofing"]
-["net-misc/partysip", "partysip.eap", "SIP-VoIP"]
-["net-misc/proxychains", "proxychains.eap", "Proxy"]
-["net-misc/raccess", "raccess.eap", "Exploit"]
-["net-misc/rdesktop", "rdesktop.eap", "Internet"]
-["net-misc/sipbomber", "sipbomber.eap", "SIP-VoIP"]
-["net-misc/sipp", "sipp.eap", "SIP-VoIP"]
-["net-misc/siproxd", "siproxd.eap", "SIP-VoIP"]
-["net-misc/sipsak", "sipsak.eap", "SIP-VoIP"]
-["net-misc/tightvnc", "tightvnc.eap", "Internet"]
-["net-misc/tinc", "tinc.eap", "Internet"]
-["net-misc/tor", "tor.eap", "Proxy"]
-["net-misc/tsclient", "tsclient.eap", "Internet"]
-["net-misc/voipong", "voipong.eap", "SIP-VoIP"]
-["net-nds/gq", "gq.eap", "Analyzer"]
-["net-nds/lat", "lat.eap", "Analyzer"]
-["net-p2p/bittorrent", "bittorrent.eap", "Internet"]
-["net-p2p/gnome-btdownload", "gnome-btdownload.eap", "Internet"]
-["net-proxy/3proxy", "3proxy.eap", "Proxy"]
-["net-proxy/httpush", "httpush.eap", "Proxy"]
-["net-proxy/paros", "paros.eap", "Proxy"]
-["net-proxy/privoxy", "privoxy.eap", "Proxy"]
-["net-proxy/webscarab", "webscarab.eap", "Proxy"]
-["net-wireless/aircrack-ng", "aircrack-ng.eap airdecap-ng.eap aireplay-ng.eap airodump-ng.eap", "Wireless/Aircrack-Suite"]
-["net-wireless/airsnort", "airsnort.eap", "Wireless"]
-["net-wireless/airtraf", "airtraf.eap", "Wireless"]
-["net-wireless/btscanner", "btscanner.eap", "Bluetooth"]
-["net-wireless/gkismet", "gkismet.eap gkismetauto.eap", "Wireless"]
-["net-wireless/gnome-bluetooth", "gnome-bluetooth.eap", "Bluetooth"]
-["net-wireless/kismet", "kismet.eap kismetauto.eap", "Wireless"]
-["net-wireless/wavemon", "wavemon.eap", "Wireless"]
-["net-wireless/waveselect", "waveselect.eap", "Wireless"]
-["net-wireless/wepattack", "wepattack.eap", "Wireless"]
-["net-wireless/wepdecrypt", "wepdecrypt.eap", "Wireless"]
-["net-wireless/wifi-radar", "wifi-radar.eap", "Wireless"]
-["net-wireless/wifiscanner", "wifiscanner.eap", "Wireless"]
-["net-wireless/wifitap", "wifitap.eap", "Wireless"]
-["net-wireless/wpa_supplicant", "wpa_supplicant.eap", "Wireless"]
-["net-www/apache", "apache.eap", "Services"]
+DB=[["net-analyzer/absinthe", "absinthe.eap", "Database"],
+    ["net-analyzer/aimsniff", "aimsniff.eap", "Analyzer"],
+    ["net-analyzer/amap", "amap.eap", "Fingerprint"],
+    ["net-analyzer/arpwatch", "arpwatch.eap", "Analyzer"],
+    ["net-analyzer/authforce", "authforce.eap", "Cracker-Bruter"],
+    ["net-analyzer/autoscan", "autoscan.eap", "Scanners"],
+    ["net-analyzer/bmon", "bmon.eap", "Analyzer"],
+    ["net-analyzer/cheops-ng", "cheops-ng.eap" "Scanners"],
+    ["net-analyzer/dnsa", "dnsa.eap", "Analyzer"],
+    ["net-analyzer/dnsenum", "dnsenum.eap", "Analyzer"],
+    ["net-analyzer/driftnet", "driftnet.eap", "Analyzer"],
+    ["net-analyzer/dsniff", "dsniff.eap", "MITM"],
+    ["net-analyzer/ethereal", "ethereal.eap", "Analyzer"],
+    ["net-analyzer/ettercap", "ettercap.eap", "MITM"],
+    ["net-analyzer/firewalk", "firewalk.eap", "Scanners"],
+    ["net-analyzer/fragroute", "fragroute.eap", "Forging-Spoofing"],
+    ["net-analyzer/ftester", "ftester.eap", "Stress"],
+    ["net-analyzer/gnome-nettool", "gnome-nettool.eap", "Analyzer"],
+    ["net-analyzer/gspoof", "gspoof.eap", "Forging-Spoofing"],
+    ["net-analyzer/honeyd", "honeyd.eap", "Misc"],
+    ["net-analyzer/hping", "hping.eap", "Forging-Spoofing"],
+    ["net-analyzer/hunt", "hunt.eap", "Forging-Spoofing"],
+    ["net-analyzer/hydra", "hydra.eap", "Cracker-Bruter"],
+    ["net-analyzer/ike-scan", "ike-scan.eap", "Scanners"],
+    ["net-analyzer/isic", "isic.eap", "Forging-Spoofing"],
+    ["net-analyzer/macchanger", "macchanger.eap", "Misc"],
+    ["net-analyzer/mbrowse", "mbrowse.eap", "Analyzer"],
+    ["net-analyzer/metacoretex-ng", "metacoretex-ng.eap", "Database"],
+    ["net-analyzer/metasploit", "metasploit.eap", "Exploits"],
+    ["net-analyzer/nbtscan", "nbtscan.eap", "Scanners"],
+    ["net-analyzer/nessus", "nessus.eap", "Scanners"],
+    ["net-analyzer/net-snmp", "net-snmp.eap", "Analyzer"],
+    ["net-analyzer/netdude", "netdude.eap", "Analyzer"],
+    ["net-analyzer/netwag", "netwag.eap", "Forging-Spoofing"],
+    ["net-analyzer/nikto", "nikto.eap", "Scanners"],
+    ["net-analyzer/nmap", "nmap.eap", "Scanners"],
+    ["net-analyzer/nmbscan", "nmbscan.eap", "Scanners"],
+    ["net-analyzer/ntop", "ntop.eap", "Analyzer"],
+    ["net-analyzer/ntp-fingerprint", "ntp-fingerprint.eap", "Fingerprint"],
+    ["net-analyzer/p0f", "p0f.eap", "Fingerprint",
+    ["net-analyzer/packit", "packit.eap", "Forging-Spoofing"],
+    ["net-analyzer/paketto", "paketto-linkcat.eap paketto-minewt.eap paketto-paratrace.eap paketto-scanrand.eap", "Forging-Spoofing"],
+    ["net-analyzer/rain", "rain.eap", "Forging-Spoofing"],
+    ["net-analyzer/sara", "sara.eap", "Scanners"],
+    ["net-analyzer/scanssh", "scanssh.eap", "Fingerprint"],
+    ["net-analyzer/siphon", "siphon.eap", "Fingerprint"],
+    ["net-analyzer/smtpmap", "smtpmap.eap", "Fingerprint"],
+    ["net-analyzer/snmpenum", "snmpenum.eap", "Analyzer"],
+    ["net-analyzer/snort", "snort.eap", "Misc"],
+    ["net-analyzer/sqlat", "sqlat.eap", "Database"],
+    ["net-analyzer/sqlbf", "sqlbf.eap", "Database"],
+    ["net-analyzer/sqlinject", "sqlinject.eap", "Database"],
+    ["net-analyzer/tcptraceroute", "tcptraceroute.eap", "Analyzer"],
+    ["net-analyzer/thcrut", "thcrut.eap", "Analyzer"],
+    ["net-analyzer/traceroute", "traceroute.eap", "Analyzer"],
+    ["net-analyzer/upnpscan", "upnpscan.eap", "Scanners"],
+    ["net-analyzer/xprobe", "xprobe.eap", "Fingerprint"],
+    ["net-analyzer/yersinia", "yersinia.eap", "Exploit"],
+    ["net-analyzer/zodiac", "zodiac.eap", "Analyzer"],
+    ["net-firewall/firehol", "firehol.eap", "Misc"],
+    ["net-firewall/fwbuilder", "fwbuilder.eap", "Misc"],
+    ["net-ftp/ftpd", "ftpd.eap", "Services"],
+    ["net-ftp/oftpd", "oftpd.eap", "Services"],
+    ["net-im/gaim", "gaim.eap", "Misc"],
+    ["net-im/skype", "skype.eap", "Internet"],
+    ["net-irc/irssi", "irssi.eap", "Internet"],
+    ["net-irc/xchat", "xchat.eap", "Internet"],
+    ["net-misc/bridge-utils", "bridge-utils.eap", "MITM"],
+    ["net-misc/curl", "curl.eap", "Analyzer"],
+    ["net-misc/dhcpcd", "dhcpcd.eap", "Misc"],
+    ["net-misc/etherwake", "etherwake.eap", "Misc"],
+    ["net-misc/ipsorcery", "ipsorcery.eap", "Forging-Spoofing"],
+    ["net-misc/minisip", "minisip.eap", "SIP-VoIP"],
+    ["net-misc/nemesis", "nemesis.eap", "Forging-Spoofing"],
+    ["net-misc/partysip", "partysip.eap", "SIP-VoIP"],
+    ["net-misc/proxychains", "proxychains.eap", "Proxy"],
+    ["net-misc/raccess", "raccess.eap", "Exploit"],
+    ["net-misc/rdesktop", "rdesktop.eap", "Internet"],
+    ["net-misc/sipbomber", "sipbomber.eap", "SIP-VoIP"],
+    ["net-misc/sipp", "sipp.eap", "SIP-VoIP"],
+    ["net-misc/siproxd", "siproxd.eap", "SIP-VoIP"],
+    ["net-misc/sipsak", "sipsak.eap", "SIP-VoIP"],
+    ["net-misc/tightvnc", "tightvnc.eap", "Internet"],
+    ["net-misc/tinc", "tinc.eap", "Internet"],
+    ["net-misc/tor", "tor.eap", "Proxy"],
+    ["net-misc/tsclient", "tsclient.eap", "Internet"],
+    ["net-misc/voipong", "voipong.eap", "SIP-VoIP"],
+    ["net-nds/gq", "gq.eap", "Analyzer"],
+    ["net-nds/lat", "lat.eap", "Analyzer"],
+    ["net-p2p/bittorrent", "bittorrent.eap", "Internet"],
+    ["net-p2p/gnome-btdownload", "gnome-btdownload.eap", "Internet"],
+    ["net-proxy/3proxy", "3proxy.eap", "Proxy"],
+    ["net-proxy/httpush", "httpush.eap", "Proxy"],
+    ["net-proxy/paros", "paros.eap", "Proxy"],
+    ["net-proxy/privoxy", "privoxy.eap", "Proxy"],
+    ["net-proxy/webscarab", "webscarab.eap", "Proxy"],
+    ["net-wireless/aircrack-ng", "aircrack-ng.eap airdecap-ng.eap aireplay-ng.eap airodump-ng.eap", "Wireless/Aircrack-Suite"],
+    ["net-wireless/airsnort", "airsnort.eap", "Wireless"],
+    ["net-wireless/airtraf", "airtraf.eap", "Wireless"],
+    ["net-wireless/btscanner", "btscanner.eap", "Bluetooth"],
+    ["net-wireless/gkismet", "gkismet.eap gkismetauto.eap", "Wireless"],
+    ["net-wireless/gnome-bluetooth", "gnome-bluetooth.eap", "Bluetooth"],
+    ["net-wireless/kismet", "kismet.eap kismetauto.eap", "Wireless"],
+    ["net-wireless/wavemon", "wavemon.eap", "Wireless"],
+    ["net-wireless/waveselect", "waveselect.eap", "Wireless"],
+    ["net-wireless/wepattack", "wepattack.eap", "Wireless"],
+    ["net-wireless/wepdecrypt", "wepdecrypt.eap", "Wireless"],
+    ["net-wireless/wifi-radar", "wifi-radar.eap", "Wireless"],
+    ["net-wireless/wifiscanner", "wifiscanner.eap", "Wireless"],
+    ["net-wireless/wifitap", "wifitap.eap", "Wireless"],
+    ["net-wireless/wpa_supplicant", "wpa_supplicant.eap", "Wireless"],
+    ["net-www/apache", "apache.eap", "Services"]]
