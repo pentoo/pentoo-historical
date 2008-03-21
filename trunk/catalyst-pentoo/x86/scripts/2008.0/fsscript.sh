b75b2039f5b4e5c15cd8f2657437af120400a248
grimmlin <grimmlin@users.noreply.github.com>
2008-03-21 15:14:53 +0000

    * Removed the kernel cleaning

--- trunk/catalyst-pentoo/x86/scripts/2008.0/fsscript.sh
+++ trunk/catalyst-pentoo/x86/scripts/2008.0/fsscript.sh
@@ -3,23 +3,19 @@
 # Purge the uneeded locale, should keeps only en
 localepurge
 
+if [[ -e /etc/conf.d/clock ]]
+then
+	sed -i -e 's/#TIMEZONE="Factory"/TIMEZONE="UTC"/' /etc/conf.d/clock
+fi
+
 # Runs the incredible menu generator
 genmenu.py -v
-env-update && source /etc/profile
 
 # Fix the root login by emptying the root password. No ssh will be allowed until 'passwd root'
 sed -i -e 's/^root:\*:/root::/' /etc/shadow
 
 # compile mingw32
 crossdev i686-mingw32
-
-#clean out kernel and make prepare for future modules support
-cd /usr/src/linux
-mv .config ../
-make clean
-make mrproper
-mv ../.config ./
-make prepare
 
 cd /opt/
 mkdir exploits/packetstorm -p
